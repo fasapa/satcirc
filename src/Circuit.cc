9f4909bdcb2aeff4f60001c2b43d1085afcc6bfc
Fabricio S. Paranhos <paranhos.s.f@gmail.com>
2018-06-05 13:46:52 -0300

    Removed this from classes implementation.

--- src/Circuit.cc
+++ src/Circuit.cc
@@ -12,45 +12,43 @@ using namespace SATCirc;
 
 // Components
 // Constructors
-Component::Component(string name) { this->_name = name; }
+Component::Component(string name) { _name = name; }
 Component::Component(string name, vector<Var> in, vector<Var> out) {
-  this->_name = name; this->_inputs = in; this->_outputs = out;
+  _name = name; _inputs = in; _outputs = out;
 }
 
 // Methods
-string      Component::name()    const { return this->_name;    }
-vector<Var> Component::inputs()  const { return this->_inputs;  }
-vector<Var> Component::outputs() const { return this->_outputs; }
+string      Component::name()    const { return _name;    }
+vector<Var> Component::inputs()  const { return _inputs;  }
+vector<Var> Component::outputs() const { return _outputs; }
 
-void Component::addInput(Var v)  { this->_inputs.push_back(v);  }
-void Component::addOutput(Var v) { this->_outputs.push_back(v); }
+void Component::addInput(Var v)  { _inputs.push_back(v);  }
+void Component::addOutput(Var v) { _outputs.push_back(v); }
 
-void Component::addInput(vector<Var> v)
-{ this->_inputs.insert(_inputs.begin(), v.begin(), v.end()); }
-void Component::addOutput(vector<Var> v)
-{ this->_outputs.insert(_outputs.begin(), v.begin(), v.end()); }
+void Component::addInput(vector<Var> v)  { _inputs.insert(_inputs.begin(), v.begin(), v.end()); }
+void Component::addOutput(vector<Var> v) { _outputs.insert(_outputs.begin(), v.begin(), v.end()); }
 
 // Circuit
 // Constructors
-Circuit::Circuit(string name) { this->_name = name; }
+Circuit::Circuit(string name) { _name = name; }
 Circuit::Circuit(string name, vector<Var> in, vector<Var> out, vector<Component> c) {
-  this->_name = name; this->_input = in; this->_output = out; this->_components = c;
+  _name = name; _input = in; _output = out; _components = c;
 }
 
 // Methods
-bool              Circuit::empty()      const { return this->_components.empty(); }
-string            Circuit::name()       const { return this->_name;               }
-vector<Var>       Circuit::inputs()     const { return this->_input;              }
-vector<Var>       Circuit::outputs()    const { return this->_output;             }
-vector<Var>       Circuit::internal()   const { return this->_internal;           }
-vector<Component> Circuit::components() const { return this->_components;         }
+bool              Circuit::empty()      const { return _components.empty(); }
+string            Circuit::name()       const { return _name;               }
+vector<Var>       Circuit::inputs()     const { return _input;              }
+vector<Var>       Circuit::outputs()    const { return _output;             }
+vector<Var>       Circuit::internal()   const { return _internal;           }
+vector<Component> Circuit::components() const { return _components;         }
 
-void Circuit::addInput(Var v)           { this->_input.push_back(v);      }
-void Circuit::addOutput(Var v)          { this->_output.push_back(v);     }
-void Circuit::addComponent(Component c) { this->_components.push_back(c); }
+void Circuit::addInput(Var v)           { _input.push_back(v);      }
+void Circuit::addOutput(Var v)          { _output.push_back(v);     }
+void Circuit::addComponent(Component c) { _components.push_back(c); }
 
-void Circuit::addInput(vector<Var> v)  { this->_input.insert(_input.end(), v.begin(), v.end());   }
-void Circuit::addOutput(vector<Var> v) { this->_output.insert(_output.end(), v.begin(), v.end()); }
+void Circuit::addInput(vector<Var> v)  { _input.insert(_input.end(), v.begin(), v.end());   }
+void Circuit::addOutput(vector<Var> v) { _output.insert(_output.end(), v.begin(), v.end()); }
 void Circuit::addComponent(vector<Component> c) {
-  this->_components.insert(_components.end(), c.begin(), c.end());
+  _components.insert(_components.end(), c.begin(), c.end());
 }
--- src/Var.cc
+++ src/Var.cc
@@ -11,37 +11,37 @@ using namespace SATCirc;
 
 // Constructors
 Var::Var(string name) {
-  this->_hasName = true; this->_hasNumber = false;
-  this->_name = name; this->_number = 0;
-  this->_sign = VarSign::NONE;
+  _hasName = true; _hasNumber = false;
+  _name = name; _number = 0;
+  _sign = VarSign::NONE;
 }
 
 Var::Var(size_t number, VarSign sign) {
-  this->_hasName = false; this->_hasNumber = true;
-  this->_name = ""; this->_number = number;
-  this->_sign = sign;
+  _hasName = false; _hasNumber = true;
+  _name = ""; _number = number;
+  _sign = sign;
 }
 
 Var::Var(string name, size_t number, VarSign sign) {
-  this->_hasName = this->_hasNumber = true;
-  this->_name = name; this->_number = number;
-  this->_sign = sign;
+  _hasName = _hasNumber = true;
+  _name = name; _number = number;
+  _sign = sign;
 }
 
 // Methods
-size_t  Var::number()    const { return this->_number;    }
-string  Var::name()      const { return this->_name;      }
-bool    Var::hasName()   const { return this->_hasName;   }
-bool    Var::hasNumber() const { return this->_hasNumber; }
-VarSign Var::sign()      const { return this->_sign;      }
+size_t  Var::number()    const { return _number;    }
+string  Var::name()      const { return _name;      }
+bool    Var::hasName()   const { return _hasName;   }
+bool    Var::hasNumber() const { return _hasNumber; }
+VarSign Var::sign()      const { return _sign;      }
 
 void Var::assocNumber(size_t number, VarSign sign) {
-  this->_hasNumber = true;
-  this->_hasNumber = number;
-  this->_sign = sign;
+  _hasNumber = true;
+  _hasNumber = number;
+  _sign = sign;
 }
 
 void Var::assocName(string name) {
-  this->_hasName = true;
-  this->_name = name;
+  _hasName = true;
+  _name = name;
 }
